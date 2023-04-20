23fda4328039745d5e79d6f2cd821a269fa6d593
Lee In Hwa <leeih@mih.co.kr>
2023-04-20 16:23:50 +0900

    [강의 수강] page 이동 router를 사용한 실습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Homepage from "./page/Homepage";
+import Aboutpage from "./page/Aboutpage";
+import ProductPage from "./page/ProductPage";
+import ProductDetailPage from "./page/ProductDetailPage";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./page/LoginPage";
+import UserPage from "./page/UserPage";
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(true);
+
+  // component
+  const PrivateRoute = () => {
+    return authenticate === true ? <UserPage /> : <Navigate to="/login" />;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<Aboutpage />}></Route>
+        <Route path="/products" element={<ProductPage />}></Route>
+        <Route path="/products/:id" element={<ProductDetailPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/user" element={<PrivateRoute />}></Route>
+      </Routes>
     </div>
   );
 }
